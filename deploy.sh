e98a0be3b8bccdcba460b39a6f2bbc1badd55dc7
blu5ir <killerdinu5031@gmail.com>
2026-06-26 10:06:32 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-echo "--- Pipeline Stage: Deployment ---"
-echo "Creating Production Directory..."
-mkdir -p dist/
-echo "Copying files to production..."
-cp app.py requirements.txt dist/
-echo "Deployment successful! App 'deployed' to ./dist folder."
+
+echo "Deploying application..."
+
+mkdir -p production
+
+cp -r . production/
+
+echo "Deployment Successful!"
